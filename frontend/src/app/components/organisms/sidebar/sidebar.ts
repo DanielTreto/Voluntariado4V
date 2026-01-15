b506f99fa00bcf6ad3605e72ae3d56b4369e3e1c
Markel Alvarado <markel.alvarado@gmail.com>
2026-01-15 09:43:19 +0100

    style(frontend): rename app title and remove sidebar profile

--- frontend/src/app/components/organisms/sidebar/sidebar.ts
+++ frontend/src/app/components/organisms/sidebar/sidebar.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarLinkComponent } from '../../molecules/sidebar-link/sidebar-link';
-import { UserProfileComponent } from '../../molecules/user-profile/user-profile';
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, SidebarLinkComponent, UserProfileComponent],
+  imports: [CommonModule, SidebarLinkComponent],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
 })
